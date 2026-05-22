6e92e57ae610174df96a1c33b425a37b121f7a0f
fgiral000 <fa.giral@alumnos.upm.es>
2026-05-22 15:42:41 -0400

    Add experimental/nn/aerojepa subpackage skeleton
    Create an empty subpackage for the AeroJEPA reusable building blocks
    (attention blocks, geometry tokenizer, context/target encoders, decoder,
    predictor) that land in subsequent commits. Establishes the SPDX
    license header, module docstring, and ``__all__`` placeholder so that
    follow-up commits only need to register new public symbols.
    
    Signed-off-by: fgiral000 <fa.giral@alumnos.upm.es>

---
+++ physicsnemo/experimental/nn/aerojepa/__init__.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 - 2026 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""AeroJEPA neural network building blocks (experimental).
+
+Reusable layers for the AeroJEPA framework: attention blocks, geometry
+tokenizer, context and target encoders, decoder, and predictor. These are
+exposed for composition by users who want to build JEPA-style architectures
+for steady 3D aerodynamic surrogate modeling independently of the full
+``AeroJEPA`` model class.
+
+API stability: experimental. Names and signatures may change between releases
+until the design graduates out of ``physicsnemo.experimental``.
+
+References
+----------
+Giral et al., "AeroJEPA: Learning Semantic Latent Representations for
+Scalable 3D Aerodynamic Field Modeling", preprint arXiv:2605.05586 (2026).
+"""
+
+__all__ = []
